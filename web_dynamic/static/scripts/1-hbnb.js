a19da495b135592e3f6f74c493adfd4355b2897d
Kevin_Koech <kevinkoech@proton.me>
2023-11-04 13:30:02 +0300

    fix semistandard errors

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,30 +1,30 @@
 $(document).ready(function () {
-    // Initialize an empty array to store checked amenity names
-    var checkedAmenities = [];
+  // Initialize an empty array to store checked amenity names
+  const checkedAmenities = [];
 
-    // Function to update the list of checked amenities
-    function updateAmenitiesList() {
-        var amenitiesList = checkedAmenities.join(", ");
-        // Update the text of the <h4> tag inside the <div> with the class "amenities"
-        $(".amenities h4").text(amenitiesList);
+  // Function to update the list of checked amenities
+  function updateAmenitiesList () {
+    const amenitiesList = checkedAmenities.join(', ');
+    // Update the text of the <h4> tag inside the <div> with the class "amenities"
+    $('.amenities h4').text(amenitiesList);
+  }
+
+  // Listen for changes on input checkboxes
+  $('input[type=checkbox][name=amenity]').change(function () {
+    const amenityName = $(this).data('name'); // Get the Amenity name from the data-name attribute
+
+    if ($(this).is(':checked')) {
+      // If the checkbox is checked, add the Amenity name to the array
+      checkedAmenities.push(amenityName);
+    } else {
+      // If the checkbox is unchecked, remove the Amenity name from the array
+      const index = checkedAmenities.indexOf(amenityName);
+      if (index !== -1) {
+        checkedAmenities.splice(index, 1);
+      }
     }
 
-    // Listen for changes on input checkboxes
-    $("input[type=checkbox][name=amenity]").change(function () {
-        var amenityName = $(this).data("name"); // Get the Amenity name from the data-name attribute
-
-        if ($(this).is(":checked")) {
-            // If the checkbox is checked, add the Amenity name to the array
-            checkedAmenities.push(amenityName);
-        } else {
-            // If the checkbox is unchecked, remove the Amenity name from the array
-            var index = checkedAmenities.indexOf(amenityName);
-            if (index !== -1) {
-                checkedAmenities.splice(index, 1);
-            }
-        }
-
-        // Update the list of checked amenities
-        updateAmenitiesList();
-    });
+    // Update the list of checked amenities
+    updateAmenitiesList();
+  });
 });
